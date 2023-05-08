df5500d259a327d1274705ae00adf2c6dc1f6140
manuelnones <manuelnones99@yahoo.com>
2023-05-08 16:06:54 +0200

    add bunus exercise

--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -10,11 +10,9 @@
 
 <body>
 
-    <a href="/pagina1">pagina 1</a>
-    <a href="/pagina2">pagina 2</a>
-    <a href="/pagina3">pagina 3</a>
+    <a href="/">home</a>
 
-    <h1>{{ $saluta }}</h1>
+    <h1>{{ $titlePage }}</h1>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function() {
+Route::get('/', function() {
 
     $saluta = "Hello World";
 
     return view('home', compact('saluta'));
+});
+
+Route::get('/pagina1', function(){
+
+    $titlePage = "Questa è la pagina 1";
+
+    return view('/pagina1', compact('titlePage'));
+});
+
+Route::get('/pagina2', function(){
+
+    $titlePage = "Questa è la pagina 2";
+
+    return view('/pagina2', compact('titlePage'));
+});
+
+Route::get('/pagina3', function(){
+
+    $titlePage = "Questa è la pagina 3";
+
+    return view('/pagina3', compact('titlePage'));
 });
